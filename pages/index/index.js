519a25b474db418d9acfe42ceb26aa56a0729fb2
wanggang <git@gitlab.zank.mobi:gaoyan/zank-admin.git>
2017-07-10 20:53:54 +0800

    获取微信用户基本信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,38 +1,45 @@
 Page({
+    scanQrcode: function() {
+        wx.scanCode({
+            onlyFromCamera: true,
+            success: function(_res) {
+                if (_res.errMsg == 'scanCode:ok') {
+                    wx.request({
+                        url: _res.result,
+                        method: 'GET',
+                        success: function(res) {
+                            if (res.result) { //能查到订单--->直接跳到成功页面
+                                wx.navigateTo({
+                                    url: '../again/again',
+                                })
+                            } else {
+                                if (res.msg) { //没有订单,但是有错误 直接弹出错误信息
+                                    wx.showModal({
+                                        title: '提示',
+                                        showCancel: false,
+                                        content: res.msg
+                                    });
+                                } else { //没有订单也没有错误,直接跳转到待支付的页面,并生成一个待支付的订单
+                                    var devCode = res.data.devCode || 'da327059e71';
+                                    wx.navigateTo({
+                                        url: '../vr/vr?devCode=' + devCode,
+                                    })
+                                }
+                            }
+                        }
+                    })
+                }
+            },
+            fail: function(res) {
+                wx.showModal({
+                    title: '提示',
+                    showCancel: false,
+                    content: "识别失败,请关闭后重试.",
+                });
+            },
+            complete: function(res) {
 
-  goToVr:function(){
-    wx.navigateTo({
-      url: '../vr/vr',
-    })
-  },
-
-  goToSao:function(){
-    wx.scanCode({
-      success: (res) => {
-        console.log(res)
-      }
-    })
-  },
-  data:{
-    tit: "速度与激情",
-    price: "9.00",
-    num: 123456
-  },
-  onLoad:function(){
-      console.log('***************');
-       wx.login({
-        success: function (code) {
-          console.log(code);
-          wx.getUserInfo({
-            success: function (res) {
-              console.log('##########');
-              console.log(res);
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
             }
-          })
-        }
-      })
-  }
-
+        });
+    }
 })
--- pages/vr/vr.js
+++ pages/vr/vr.js
@@ -1,27 +1,90 @@
 Page({
+    goToPay: function() {
+        wx.requestPayment({
+            'timeStamp': '',
+            'nonceStr': '',
+            'package': '',
+            'signType': 'MD5',
+            'paySign': '',
+            'success': function(res) {},
+            'fail': function(res) {}
+        })
+    },
+    data: {
+        price: "0.01",
+        devCode: "",
+        userInfo: {}
+    },
+    onLoad: function(option) {
+        var _this = this;
+        _this.setData({ devCode: option.devCode + '' });
 
-  goToPay:function(){
-    wx.requestPayment({
-      'timeStamp': '',
-      'nonceStr': '',
-      'package': '',
-      'signType': 'MD5',
-      'paySign': '',
-      'success':function(res){
-      },
-      'fail':function(res){
-      }
-    })
-  },
-  goToPay1:function(){
-    wx.navigateTo({
-      url: '../pay/pay',
-    })
-  },
-  data:{
-    tit: "速度与激情",
-    price: "9.00",
-    num: 12345
-  }
+        //获取微信信息
+        wx.login({
+            success: function(res) {
+                if (res.code) {
+                    wx.request({
+                        url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx6988b48707511cc1&secret=164d368385cfb0120551ae2cc541eed2&js_code=' + res.code + '&grant_type=authorization_code',
+                        // data: {
+                        //     code: res.code
+                        // },
+                        success: function(_res) {
+                            wx.showModal({
+                                title: '提示',
+                                showCancel: false,
+                                content: JSON.stringify(_this.data),
+                            });
+                            var openId = _res.data.openId;
+                            wx.request({
+                                url: 'https://vr.mtscrm.com/api/v1/order/save',
+                                data: {
+                                    movieKey: openId,
+                                    realFee: _this.data.price,
+                                    devCode: _this.data.devCode
+                                },
+                                method: 'POST',
+                                succe: function(res) {
+                                    //获取微信支付的信息 
+                                    wx.showModal({
+                                        title: '提示-success',
+                                        showCancel: false,
+                                        content: JSON.stringify(_res),
+                                    });
 
+                                },
+                                fail: function(res) {
+                                    wx.showModal({
+                                        title: '提示-err',
+                                        showCancel: false,
+                                        content: JSON.stringify(_res),
+                                    });
+                                }
+                            });
+                            //创建订单信息   
+                        },
+                        fail: function(_res) {
+                            wx.showModal({
+                                title: '提示-err',
+                                showCancel: false,
+                                content: JSON.stringify(_res),
+                            });
+                        }
+                    })
+                } else {
+
+                }
+                // wx.getUserInfo({
+                //     success: function(res) {
+                //         _this.setData({ userInfo: res.userInfo + '' });
+                //         wx.showModal({
+                //             title: '提示',
+                //             showCancel: false,
+                //             content: JSON.stringify(res.userInfo),
+                //         });
+                //     }
+                // })
+            }
+        })
+
+    }
 })
